a19a95f8d1e115ff30860aacd08313037e842181
Hutsmeister <116343505+Hutsmeister@users.noreply.github.com>
2022-11-04 09:54:05 +0100

    Tafel klasse

--- Framework2122-VWO/Spel/Tafel.java
+++ Framework2122-VWO/Spel/Tafel.java
@@ -1,14 +1,13 @@
+package Spel;
+import java.awt.Graphics2D;
+import java.awt.Color;
 
-public class Tafel{
-public Tafel(int ){
-
-
-
-
-}
-
-
-
-
-
+public class Tafel extends SpelObject{
+    public Tafel(int x, int y, int breedte, int hoogte, image Plaatje){
+        super(x,y,breedte, hoogte);
+        this.plaatje = "plaatjes/tafel.png"
+    }
+    public void teken(Graphics2D g){
+        g.fillRect((int)x, (int)y,breedte, hoogte);
+    }
 }
